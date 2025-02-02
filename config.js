bb30eeb20ec7b7940ba7b25abaa08b06f4940c1f
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-02 16:57:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
+}
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID || "WcpHQZxL#mJp0BbGf_sDMTXetY8zL7yrafwIo2zP0WFsYuvMIUS4",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:SvcCrSyNAzCyuTHJjLyYgDsAjEHyslzl@autorack.proxy.rlwy.net:21535",
+  OWNER_NUM: process.env.OWNER_NUM || "94764582504",
+};
