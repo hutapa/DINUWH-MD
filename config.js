9c4a10e79c6b588c65ab864dcb7a005f686618e8
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-03 16:53:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "kNoDiQQT#aJmyx45Nrkjr8AJom1_bwViVk4DJ40rNGnzNo5yahLc",
+  SESSION_ID: process.env.SESSION_ID || "33gW1SqY#87ZsRHDkMF0R4578o24tNZ7AsiMa4Yyz3Oehp2MUSMs",
   MONGODB: process.env.MONGODB || "mongodb://mongo:cDcUmTPFlGXoMrEwOkiPaJkvYYhBKveq@junction.proxy.rlwy.net:48589",
   OWNER_NUM: process.env.OWNER_NUM || "94720244981",
 };
