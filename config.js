52554abb823a2c89fff752dfe4f7be3c356f8537
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-03 20:39:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "33gW1SqY#87ZsRHDkMF0R4578o24tNZ7AsiMa4Yyz3Oehp2MUSMs",
   MONGODB: process.env.MONGODB || "mongodb://mongo:cDcUmTPFlGXoMrEwOkiPaJkvYYhBKveq@junction.proxy.rlwy.net:48589",
   OWNER_NUM: process.env.OWNER_NUMBER || "94764582504",
-  MODE: process.env.MODE || "private"
+  MODE: process.env.MODE || "public"
 };
