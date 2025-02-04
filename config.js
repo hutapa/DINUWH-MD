e8e4a60bbe1fe0068bb03f4045c2c5b20a9b651b
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-04 12:55:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ if (fs.existsSync("config.env"))
 function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
-module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "33gW1SqY#87ZsRHDkMF0R4578o24tNZ7AsiMa4Yyz3Oehp2MUSMs",
+module.exports = {SESSION_ID: process.env.SESSION_ID || "33gW1SqY#87ZsRHDkMF0R4578o24tNZ7AsiMa4Yyz3Oehp2MUSMs",
   MONGODB: process.env.MONGODB || "mongodb://mongo:bLIlxRxlCPmoZzLlfKqPrAoCUIcIGxIl@viaduct.proxy.rlwy.net:45622",
   OWNER_NUM: process.env.OWNER_NUM || "94764582504",
 };
+
