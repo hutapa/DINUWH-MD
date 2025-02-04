5a8f17c0b70774525bd4a00e32c3c9627fe97739
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-04 06:24:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,11 @@ async function connectToWA() {
         : mek.message;
     if (
       mek.key &&
-      mek.key.remoteJid === "status@broadcast") return  
+      mek.key.remoteJid === "status@broadcast") &&
+      config.AUTO_READ_STATUS === "true"
+      ) {
+    await.robin([mek.key]);
+  }
     
     const m = sms(robin, mek);
     const type = getContentType(mek.message);
