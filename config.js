d2014230c981f8ce366e6e45d8461ca959809bce
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-04 19:20:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,18 @@
-const fs = require("fs");
-if (fs.existsSync("config.env"))
-  require("dotenv").config({ path: "./config.env" });
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = "true") {
-  return text === fault ? true : false;
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
 }
 module.exports = {
-  MONGODB: process.env.MONGODB || "mongodb://mongo:oBCvYzQlfblDyFchIUEfVKgscFwOeVpx@monorail.proxy.rlwy.net:40207",
-  OWNER_NUM: process.env.OWNER_NUM || "94720244981",
-  SESSION_ID: process.env.SESSION_ID || "o6En3ZSS#Lpwn7gbPa22IQC_ujyCrnKOgPAxRydXxu3a1bDggccE",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
+FOOTER: process.env.FOOTER || "Didula MD V2 💚",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
+SESSION_ID: process.env.SESSION_ID || "o6En3ZSS#Lpwn7gbPa22IQC_ujyCrnKOgPAxRydXxu3a1bDggccE",
 };
+
+
 
