d534d0b6b59ab464bf44f555b992bc86412cce61
Yutsara4max <168951295+Yutsara4max@users.noreply.github.com>
2025-02-04 11:02:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const {
 } = require("./lib/functions");
 const fs = require("fs");
 const P = require("pino");
-const config = require(".config");
+const config = require("./config");
 const qrcode = require("qrcode-terminal");
 const util = require("util");
 const easy = require("./lib/mongodb");
