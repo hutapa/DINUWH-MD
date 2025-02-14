2ea2072d9a8ab8079f0784578446b623cd1c96e1
hutapa <kawdhithaytpremum@gmail.com>
2025-02-14 15:10:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,20 +21,22 @@ const prefix = '.';
 
 const ownerNumber = ['94729787750'];
 
-//===================SESSION-AUTH============================
-if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-  if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-  const sessdata = sessionID
-  const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
-  filer.download((err, data) => {
-  if(err) throw err
-  fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-  console.log("Session downloaded ✅")
-  })})}    
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 8000;
-//================================/
+  //===================SESSION-AUTH============================
+  if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
+    if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+    const sessdata = config.SESSION_ID.split("𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=")[1];
+    const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+    filer.download((err, data) => {
+    if(err) throw err
+    fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
+    console.log("Session downloaded ✅")
+    })})}
+    
+    const express = require("express");
+    const app = express();
+    const port = process.env.PORT || 8000;
+    
+  //=============================================
 
 async function connectToWA() {
   console.log("Didula MD V2 💚 Connecting wa bot 🧬...");
