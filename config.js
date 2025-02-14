b698721031f6ae7a48790a17677f79100e5d635a
hutapa <kawdhithaytpremum@gmail.com>
2025-02-14 15:05:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "false",
+AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
 STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
 FOOTER: process.env.FOOTER || "Didula MD V2 💚",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "tztjGDSK#KFOG0fl3YTlXp1n4kZMKYTrjSaVlfmhfz2VQWwMer1M",
-OWNER_NUMBER: process.env.SESSION_ID || "94764582504",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nzZAmQCS#NxXNtSfnHtzOeBSy5XNJUAFSShy915l0ftrwen7kcMI",
+OWNER_NUMBER: process.env.SESSION_ID || "94740617415",
 PREFIX: process.env.PREFIX || "."
 };
 
